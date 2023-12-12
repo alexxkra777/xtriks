ce4c9d50fbd0b14030d839c2356d73826f7beeb6
krajuskin1 <krayushkin@gpsdozor.cz>
2023-12-12 11:02:17 +0100

    new components menu and isntall sass

---
+++ src/components/menu/Menu.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+    <div class="div">
+
+    </div>   
+
+<style lang="scss">
+    .div{
+        height: 100px;
+        width: 100px;
+        background-color: aqua;
+    }
+</style>
+  
