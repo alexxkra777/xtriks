501f0238e78169757ff25cdee47040870ae11e44
krajuskin1 <krayushkin@gpsdozor.cz>
2024-02-13 09:32:03 +0100

    fix bug delete updatestore

--- src/routes/test/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { updateArray } from '$lib/stores/arrayStore';
-
-  // Example array data
-  let arrayData = ["item1", "item2", "item3"];
-
-  // Update the store with the array data
-  updateArray(arrayData);
-</script>
